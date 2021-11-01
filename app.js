6a152ebeb6855fbdc6b1f3a508fce3c9f8418489
Igor Gnatiuk <igor.gnatiuk111@gmail.com>
2021-11-01 22:23:05 +0200

    HW4 done

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
 
-const contactsRouter = require('./routes/api/contacts')
+
+const authRouter = require('./routes/api/auth');
+const contactsRouter = require('./routes/api/contacts');
 
 const app = express()
 
@@ -12,14 +14,15 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/contacts', contactsRouter)
+app.use('/api/auth', authRouter);
+app.use('/api/contacts', contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = 'Server error' } = err
+  const { status = 500, message = 'Server error' } = err;
   res.status(status).json({ message })
 })
 
